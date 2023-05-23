e6b271c69cd66a0ec2c17889657d6fad164b86ff
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-05-23 04:39:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # note: we require errors to propagate (don't set -e)
 set -u
 
-PROJECT_NAME="grype-db"
+PROJECT_NAME="griffon-db"
 OWNER=anchore
 REPO="${PROJECT_NAME}"
 GITHUB_DOWNLOAD_PREFIX=https://github.com/${OWNER}/${REPO}/releases/download
